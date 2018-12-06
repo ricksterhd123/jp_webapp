b948af000090320a6951c9ffffdc7c3fda8934fc
up864163 <up864163@myport.ac.uk>
2018-12-06 20:49:56 +0000

    Test dbConnect is working properly

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require('utils/dbConnect.php'); ?>
 <html>
 	<head>
 		<title>Jumppack store page</title>
@@ -6,7 +7,17 @@
 	<body>
 		<h1>Jetpack</h1>
 		<h2>An all in one game launcher</h2>
-
+        <article>
+            <h3>Games list: </h3>
+            <?php 
+                $db = new dbConnection("35.205.37.34", "webapp", "password", "games");
+                $games = $db->query("SELECT * FROM games");
+                for ($row_no = 0; $row_no <= count($games); $row_no++)
+                {
+                    echo "<p>". $games[$row_no]['Title'] . "</p>";
+                }
+            ?>
+        </article>
 	</body>
 </html>
 
