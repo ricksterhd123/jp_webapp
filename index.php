be140b6cb2270d073abb9a1d0defc8cea6295787
up864163 <up864163@myport.ac.uk>
2018-12-07 17:47:12 +0000

    Lets try this instead

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 		<title>Jumppack games page</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<style>
+			article {
+				margin: 10px;
+				padding: 10px;
+				background-color: black;
+				color: white;	
+			}
+		</style>
 	</head>
 	<body>
 		<h1>Jetpack - A simple games launcher</h1>
