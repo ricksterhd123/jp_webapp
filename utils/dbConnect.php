a2270b40454b86c930f7d6f641ac11b21c065378
up864163 <up864163@myport.ac.uk>
2018-12-07 00:56:00 +0000

    maybe this will work

--- utils/dbConnect.php
+++ utils/dbConnect.php
@@ -47,7 +47,7 @@
         */
         public function query($queryString)
         {
-            assert($this->connection != false && $this->connection != NULL);        
+            assert($this->connection != false && $this->connection != NULL)        
             $queryString = $this->connection->real_escape_string($queryString);     // Escape the query string for security reasons.
             $queryResult = $this->connection->query($queryString);            // Get the result back from the query.
             $results = array();                                         // Setup array for returning back the query results.
@@ -60,7 +60,7 @@
             }
             
             // Return false when there are no results.
-            return (!empty($results) ? $results : false);
+            return (!empty($results) ? $results : false;
         }
         
         /* Close the connection once we're done */
