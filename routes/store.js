f923884dfff01ac330e1ab547e4e3d3b52fc15dc
ricksterhd123 <ricksterhd123@gmail.com>
2019-03-28 01:38:23 +0000

    api/store: Added mysql model

--- routes/store.js
+++ routes/store.js
@@ -6,10 +6,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/mysql');
 
-router.get('/', function(req, res, next) {
-	const query = "SELECT * FROM test";
-	const id = req.query.game;
-	if (id){ query = query + " WHERE userid = " + connection.escape(id); }
+router.get('/', (req, res, next) => {
 	db.listGames(req.query.game, (games) => {res.json(games);});
 });
 module.exports = router;
