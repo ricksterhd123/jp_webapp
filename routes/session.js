50ea3163b2671c31baf3b5bdd6720cc639eec9b7
ricksterhd123 <ricksterhd123@gmail.com>
2019-03-31 22:36:16 +0100

    testing without express-basic-auth

--- routes/session.js
+++ routes/session.js
@@ -2,9 +2,30 @@ const express = require('express');
 const router = express.Router();
 const MYSQL = require('../models/mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.send("Hello world").status(203);
+app.get("/", function(req, res, next){
+  // Grab the "Authorization" header.
+  var auth = req.get("authorization");
+
+  // On the first request, the "Authorization" header won't exist, so we'll set a Response
+  // header that prompts the browser to ask for a username and password.
+  if (!auth) {
+    res.set("WWW-Authenticate", "Basic realm=\"Authorization Required\"");
+    // If the user cancels the dialog, or enters the password wrong too many times,
+    // show the Access Restricted error message.
+    return res.status(401).send("Authorization Required");
+  } else {
+    // If the user enters a username and password, the browser re-requests the route
+    // and includes a Base64 string of those credentials.
+    var credentials = new Buffer(auth.split(" ").pop(), "base64").toString("ascii").split(":");
+    if (credentials[0] === "username" && credentials[1] === "password") {
+      // The username and password are correct, so the user is authorized.
+      return res.send("Access Granted!");
+    } else {
+      // The user typed in the username or password wrong.
+      return res.status(401).send("Access Denied (incorrect credentials)");
+    }
+  }
+
 });
 
 /* POST to session with credentials */
