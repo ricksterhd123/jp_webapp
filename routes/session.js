ce5675c814c133c7bc428cf104c8f186571b895d
ricksterhd123 <ricksterhd123@gmail.com>
2019-03-31 23:53:42 +0100

    Using HTTP GET instead of HTTP POST on /api/session to retrieve the JWT

--- routes/session.js
+++ routes/session.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 const MYSQL = require('../models/mysql');
 
+/* Send signed JWT once user successfully authenticated */
 router.get("/", function(req, res, next) {
     // Grab the "Authorization" header.
     var auth = req.get("authorization");
@@ -20,6 +23,7 @@ router.get("/", function(req, res, next) {
         var username = credentials[0];
         var password = credentials[1];
 
+        // Check if username and password matches in the database.
         var db = new MYSQL();
         db.getUserData(username, (result) => {
             if (!result || result.length == 0) {
@@ -42,26 +46,4 @@ router.get("/", function(req, res, next) {
         db.end();
     }
 });
-
-/* POST to session with credentials */
-
-/* TODO: https://www.npmjs.com/package/express-basic-auth
-The body of POST the request will contain username and password data but
-I think we should use the Authorization header instead.
-*/
-router.post('/', function(req, res) {
-    console.log("Username: " + req.body.username);
-    console.log("Password: " + req.body.password);
-
-    let db = new MYSQL();
-    db.getUserData(req.body.username, (result) => {
-        if (result[0].password == req.body.password) {
-            console.log("Success!");
-            res.send("Success!").status(203);
-        } else {
-            res.send("Error").status(404);
-        }
-    });
-})
-
 module.exports = router;
